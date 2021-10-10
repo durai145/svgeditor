b601c4b772e47ae2a5016f850bd0843a56ab707e
Duraimurugan Govindaraj <durai145@gmail.com>
2021-10-09 20:41:16 -0400

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# svgeditor" >> README.md
+git init
+git add --all 
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/durai145/svgeditor.git
+git push -u origin main
